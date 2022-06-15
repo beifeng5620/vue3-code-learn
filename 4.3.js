cea8881ad1cac7394a5073f145a466155f27fae5
beifeng <beifeng666@gmail.com>
2022-06-15 23:47:23 +0800

    feat ✨: 4.3 设计一个完善的响应系统
    一个用来注册副作用函数的机制，支持匿名函数

---
+++ 4.3.js
@@ -0,0 +1,50 @@
+/*
+  缺陷：副作用函数与被操作的目标字段没有建立明确的联系
+  例如：
+    当读取属性时，无论读取的是哪个属性，都会把副作用函数收集到桶里。
+    当设置属性时，无论设置的是哪个属性，也都会把桶里的副作用函数取出并执行。
+*/
+(function () {
+  // 存储副作用函数的桶
+  const bucket = new Set();
+
+  // 原始数据
+  const data = { text: "hello4.3" };
+  // 对原始数据的代理
+  const obj = new Proxy(data, {
+    // 拦截读取操作
+    get(target, key) {
+      // 将副作用函数添加effect添加到存储副作用函数的桶中
+      if (activeEffect) {
+        bucket.add(activeEffect);
+      }
+      // 返回属性值
+      return target[key];
+    },
+    // 拦截写入操作
+    set(target, key, value) {
+      // 设置属性值
+      target[key] = value;
+      // 把副作用函数从桶里取出并执行
+      bucket.forEach(fn => fn());
+      // 返回true，表示写入成功
+      return true;
+    }
+  });
+
+  // 用一个全局变量指向当前正在执行的副作用函数
+  let activeEffect = null;
+  function effect(fn) {
+    // 当调用effect时，将当前正在执行的副作用函数设置为fn
+    activeEffect = fn;
+    fn();
+  }
+
+  effect(() => {
+    console.log("effect: ", obj.text);
+  });
+
+  setTimeout(() => {
+    obj.text = "world4.3";
+  }, 1000);
+})();
